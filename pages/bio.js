101285e9ae6b5d93d5dc956736534043ab6dffbb
Riki Ramadhani <rikiramadhani08@gmail.com>
2021-12-06 05:43:49 +0700

    add web developer in bio

--- pages/bio.js
+++ pages/bio.js
@@ -2,7 +2,7 @@ const Bio = () => {
   return (
     <div className="mt-3">
       <p className="display-4 text-center">I&#39;m Riki Ramadhani</p>
-      <p className="text-center">Riki Ramadhani&#39;s bio here</p>
+      <p className="text-center">Riki Ramadhani&#39;s bio here. I am a Web Developer</p>
     </div>
   )
 }
