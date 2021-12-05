d2f8d4aaa28893c567b328117e0f18262230e2d0
Riki Ramadhani <rikiramadhani08@gmail.com>
2021-12-05 15:42:56 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from "next/head"
+import Nav from "../components/Nav"
+import '../styles/bootstrap.min.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+    <title>Next and MDX Blog</title>
+  </Head>
+  <div className="container">
+    <Nav />
+    <main>
+      <Component {...pageProps} />
+    </main>
+  </div>
+
+</>
 }
 
 export default MyApp
---
+++ pages/bio.js
@@ -0,0 +1,9 @@
+const Bio = () => {
+  return (
+    <div className="mt-3">
+      <p className="display-4 text-center">I&#39;m Riki Ramadhani</p>
+      <p className="text-center">Riki Ramadhani&#39;s bio here</p>
+    </div>
+  )
+}
+export default Bio
